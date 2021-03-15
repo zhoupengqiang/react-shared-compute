3dc0c798b2838fa2dc7db36cb5bac3dd540c54e6
zhoupengqiang <pengqiangzhou@163.com>
2021-03-15 16:18:02 +0800

    feat: use closure save _deps & _value

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 export const createSharedCompute = (fn, isEqual) => {
-  
-  function sharedCompute(...args){
-    if(typeof sharedCompute._deps === 'undefined'){
-      sharedCompute._deps = args
-      return sharedCompute._value = fn(...args)
+  let _deps;
+  let _value;
+
+  function sharedCompute(...args) {
+    if (typeof _deps === "undefined") {
+      _deps = args;
+      return (_value = fn(...args));
     }
 
-    if(typeof isEqual === 'function' && isEqual(sharedCompute._deps, args)){
-      return sharedCompute._value
+    if (typeof isEqual === "function" && isEqual(_deps, args)) {
+      return _value;
     }
 
-    if(sharedCompute._deps.some((v, idx) => v !== args[idx])){
-      sharedCompute._deps = args
-      sharedCompute._value = fn(...args)
+    if (_deps.some((v, idx) => v !== args[idx])) {
+      _deps = args;
+      _value = fn(...args);
     }
-    return sharedCompute._value
+    return _value;
   }
 
-  return sharedCompute
-}
+  return sharedCompute;
+};
